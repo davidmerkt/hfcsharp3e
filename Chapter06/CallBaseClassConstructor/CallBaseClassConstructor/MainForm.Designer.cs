2f1e5dbc9dc61899d6b61377d31065e18bddff08
David Merkt <davidmerkt@users.noreply.github.com>
2016-10-13 19:17:36 -0400

    Chapter06 - Page 274

---
+++ Chapter06/CallBaseClassConstructor/CallBaseClassConstructor/MainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace CallBaseClassConstructor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnMessage.Location = new System.Drawing.Point(73, 115);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(136, 23);
+            this.btnMessage.TabIndex = 0;
+            this.btnMessage.Text = "Show Message";
+            this.btnMessage.UseVisualStyleBackColor = true;
+            this.btnMessage.Click += new System.EventHandler(this.btnMessage_Click);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnMessage;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.btnMessage);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessage;
+    }
+}
+
---
+++ Chapter06/PoolPuzzle/PoolPuzzle/PoolPuzzle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PoolPuzzle
+{
+    class PoolPuzzle
+    {
+        class Boat
+        {
+            private int length;
+
+            public void SetLength(int len)
+            {
+                length = len;
+            }
+
+            public int GetLength()
+            {
+                return length;
+            }
+
+            public virtual string Move()
+            {
+                return " drift ";
+            }
+        }
+
+        class Rowboat : Boat
+        {
+            public string RowTheBoat()
+            {
+                return " stroke ";
+            }
+        }
+
+        class Sailboat : Boat
+        {
+            public override string Move()
+            {
+                return " hoist sail ";
+            }
+        }
+
+        class TestBoats
+        {
+            public static void Main()
+            {
+                string xyz = "";
+                Boat b1 = new Boat();
+                Sailboat b2 = new Sailboat();
+                Rowboat b3 = new Rowboat();
+
+                b2.SetLength(32);
+
+                xyz = b1.Move();
+                xyz += b3.Move();
+                xyz += b2.Move();
+
+                MessageBox.Show(xyz);
+            }
+        }
+    }
+}
